6b946f319b457d024b41fe6ad49b2bffdcc1b73b
VBSREELAKSHMI123 <vbsreelakshmi06@gmail.com>
2024-06-13 00:40:42 +0530

    search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddPatient from './components/AddPatient';
+import SearchPatient from './components/SearchPatient';
 
 function App() {
   return (
     <div>
       <AddPatient/>
+      <SearchPatient/>
     </div>
   );
 }
