5f5b0fc2d9ce401ca058a7d97791ea023eeea14c
VBSREELAKSHMI123 <vbsreelakshmi06@gmail.com>
2024-06-13 00:25:05 +0530

    adding patient

---
+++ src/components/AddPatient.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const AddPatient = () => {
+  return (
+    <div>
+        <div className="container">
+            <center><h1><b>ADD PATIENT</b></h1></center>
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">PATIENT NAME</label>
+                            <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">TOKEN NUMBER</label>
+                        <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">DOCTOR NAME</label>
+                        <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">DATE</label>
+                        <input type="date" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <center><button className="btn btn-success">REGISTER</button></center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddPatient
